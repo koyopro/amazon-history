36cd2225712a662fc1e822d3a9711025a68cdb31
Kohei Yoshida <ysdkhi@gmail.com>
2015-01-01 20:31:29 +0900

    金額のみ出力

--- bookmarklet.js
+++ bookmarklet.js
@@ -11,23 +11,9 @@
 var d=document;
 var s=d.createElement('script');
 s.src=(function(){
-
-	var datePattern = new RegExp("(\\d{4})年(\\d{1,2})月(\\d{1,2})日");
-	/* TSVの出力形式を指定する */
-	function formatEntry(entry) {
-		entry.date.match(datePattern);
-		var year = RegExp.$1;
-		var month = RegExp.$2; if (month.length <= 1) month = "0" + month;
-		var day = RegExp.$3; if (day.length <= 1) day = "0" + day;
-		var date = "" + year + "/" + month + "/" + day;
-		var arr = [date, entry.name, entry.author, entry.url];
-		return arr.join('\t') + "\n";
-	}
-	 
 	var total = {};
 	var year = '2014';
 	var all = false;
-	var ret = [];
 	function init(num) {
 		if(typeof num !== 'number') {
 			num = 0;
@@ -59,25 +45,9 @@ s.src=(function(){
 					_total += yen;
 				});
 				if(all) txt += '総計' + addFigure(_total) + '円分\n';
-				var content = "";
-				$.each(ret, function(i, item) {
-					content += formatEntry(item);
-				});
-				popup(content).alert(txt + 'の買い物をAmazonでしました！');
+				alert(txt + 'の買い物をAmazonでしました！');
 			}
 		});
-	}
-
-	function popup(content) {
-		var generator=window.open('','name','height=250,width=700');
-		generator.document.write('<html><head><title>Amazon to CSV</title>');
-		generator.document.write('</head><body>');
-		generator.document.write('<pre>');
-		generator.document.write(content);
-		generator.document.write('</pre>');
-		generator.document.write('</body></html>');
-		generator.document.close();
-		return generator;
 	}
  
 	function load(num) {
@@ -91,21 +61,8 @@ s.src=(function(){
 				var dom = $.parseHTML(item[2]);
 				// 注文単位のループ
 				if (dom && dom.length > 1) {
-					var text = dom[1].innerText;
-					var arr0 = $(dom).find(".a-size-base");
 					var arr1 = $(dom).find("div.a-row > span.a-size-small");
-					var arr2 = $(dom).find("div.a-row > a.a-link-normal");
 					var price = $(arr0[1]).text().match(/[0-9]/g).join('');
-					var date = $(arr0[0]).text().replace(/(^\s+|\s+$)/g, '');
-					for (var j=0; j<arr2.length; j++) {
-						var item= {};
-						item.name = $(arr2[j]).text().replace(/(^\s+|\s+$|\n|\s\s)/g, '');
-						item.path = $(arr2[j]).attr('href').replace(/(^\s+|\s+$)/g, '');
-						item.url = 'https://www.amazon.co.jp' + item.path;
-						item.date = date;
-						item.author = $(arr1[j*2]).text().replace(/(^\s+|\s+$|\n|\s\s)/g, '');
-						ret.push(item);
-					}
 					_total += (Number(price));
 				}
 			}
